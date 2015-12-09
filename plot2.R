95191c094a742702b3313a507e678f7bdcaca7b8
emiliogozo <emilio.gozo@obf.ateneo.edu>
2015-12-09 10:59:30 +0800

    add plot2.R

---
+++ plot2.R
@@ -0,0 +1,25 @@
+#the input file
+inFile <- "../household_power_consumption.txt"
+
+#read the csv
+csv <- read.csv(inFile,sep=";",na.strings="?")
+
+#add a datetime column from date and time columns
+csv$DateTime <- strptime(paste(csv$Date,csv$Time),format="%d/%m/%Y %H:%M:%S")
+
+#filter the data
+dt1 <- strptime("01/02/2007", format="%d/%m/%Y")
+dt2 <- strptime("02/02/2007 23:59:00", format="%d/%m/%Y %H:%M:%S")
+csv <- csv[csv$DateTime >= dt1 & csv$DateTime <= dt2,]
+
+#plot
+with(csv,
+	plot(DateTime,Global_active_power,type="l",
+		xlab=NA,
+		ylab="Global Active Power (kilowatts)"))
+#save as png
+dev.copy(png,"plot1.png",width=480,height=480)
+#close grDevice
+dev.off()
+
+q("no")
